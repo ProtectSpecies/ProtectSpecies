3c59409f97eeda35783e8d2d48d0a7f80b15514c
Emre Kalkan <kalkan_emre@outlook.com>
2021-03-07 11:00:14 +0300

    Additional commit:   -Profile screen

---
+++ lib/screens/profile.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+
+class Profile extends StatefulWidget {
+  @override
+  _ProfileState createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Padding(
+          padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0,0.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+                children:<Widget>[
+                    SizedBox(
+                        height:150.0,
+                        child: Icon(
+                            Icons.person,
+                            size: 100.0,
+                        ),
+                    ),
+                    Text(
+                        "Name",
+                          style: TextStyle(
+                          color: Colors.grey,
+                          letterSpacing: 2.0,
+                                ),
+                          ),
+                    SizedBox(height:10.0),
+                      Text(
+                          "Nature Lover",
+                          style: TextStyle(
+                          color: Colors.amberAccent,
+                          letterSpacing: 2.0,
+                          fontSize: 28.0,
+                          fontWeight: FontWeight.bold,
+                              ),
+                          ),
+                      SizedBox(height:30.0),
+                      Row(
+                          children:<Widget>[
+                              Icon(
+                                  Icons.email,
+                                  color:Colors.grey[400],
+                                  ),
+                              SizedBox(width:10.0),
+                              Text("nature_lover@protectspecies.com",
+                                  style: TextStyle(
+                                  color:Colors.grey[400],
+                                  fontSize: 15.0,
+                                  letterSpacing:1.0,
+                                      )
+                                  ),
+                              ],
+                          ),
+                    ],
+                ),
+            ),
+        ),
+    );
+  }
+}
