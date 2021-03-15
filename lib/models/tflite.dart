9b817fb95495222bb0459fb06b2f5c6f50693d57
Tuna Özkuşaksız <tunaozkusaksiz3@gmail.com>
2021-03-15 23:05:55 +0300

    tflitev2

--- lib/models/tflite.dart
+++ lib/models/tflite.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:tflite/tflite.dart';
+import '/screens/home/identify.dart';
+import 'dart:io';
 
-loadModel() {
-  Tflite.loadModel(
+loadModel() async {
+  await Tflite.loadModel(
     model: "assets/model_unquant.tflite",
     labels: "assets/labels.txt",
   );
 }
 
-chooseImage() {}
+runModel(File image) {
+  Tflite.runModelOnImage(
+    path: image.path,
+    numResults: 5,
+    imageMean: 127.5,
+    imageStd: 127.5,
+    threshold: 0.5,
+  );
+}
