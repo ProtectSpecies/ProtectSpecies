843256d500511fa869c2b98ef8d4fe49f95df7aa
Berken Tekin <tekin.berken01@gmail.com>
2021-06-30 09:55:27 +0300

    Revert Wrapper, no longer fully suports Org. accounts

--- lib/screens/Wrapper.dart
+++ lib/screens/Wrapper.dart
@@ -1,55 +1,22 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterapp/screens/authenticate/authenticate.dart';
 import 'package:flutterapp/screens/home/main_pages_wrapper.dart';
-import 'package:flutterapp/screens/home/organizatinal_page.dart';
 import 'package:provider/provider.dart';
 import 'package:flutterapp/models/fUser.dart';
 
-class Wrapper extends StatefulWidget {
-  @override
-  _WrapperState createState() {
-    return new _WrapperState();
-  }
-}
 
-class _WrapperState extends State<Wrapper>{
-  final FirebaseAuth auth = FirebaseAuth.instance;
-
-  String _role;
-
-  var user;
-
-  _WrapperState() {
-    user = auth.currentUser;
-    getRole(user).then((val) => setState(() {
-      _role = val;
-    }));
-  }
-
+class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
-    print(user);
     // return either home page or authentication page
+
+    final user = Provider.of<FbUser>(context);
+
     if (user == null){
       return Authenticate();
-    }
-
-    else if (_role == 'org') {
-      return MainPages();
-    }
-
-    else {
+    } else {
       return MainPages();
     }
 
   }
-
-  Future<String> getRole (var user) async {
-    DocumentReference currUser = FirebaseFirestore.instance.collection('accounts').doc(user.uid);
-    return (await currUser.get())['role'];
-  }
-
 }
